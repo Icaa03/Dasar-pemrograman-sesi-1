28c1c1c704ef7a39cd5bd1ef508e2dc07164c21f
Anisa Cikal Virgifiani <160223842+Icaa03@users.noreply.github.com>
2024-02-22 21:58:43 +0700

    Add files via upload

---
+++ Uang Kembalian.py
@@ -0,0 +1,4 @@
+uang = int(input("masukan uang anda : "))
+pengeluaran = int(input("masukan total pengeluaran anda : "))
+kembalian = uang - pengeluaran
+print(kembalian)
---
+++ Umur.py
@@ -0,0 +1,4 @@
+tahunLahir = int(input("masukan tahun lahir anda : "))
+tahun_sekarang = 2024
+umur = tahun_sekarang - tahunLahir
+print("umur saya adalah %i sekarang dan lahir pada tahun %i"%(umur, tahunLahir))
